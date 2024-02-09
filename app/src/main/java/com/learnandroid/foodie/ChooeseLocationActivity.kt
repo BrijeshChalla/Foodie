74e1a5f1c58e171c640889b60686f2b6c883a0cd
Brijesh Challa <challabrijesh0@gmail.com>
2024-02-09 18:49:02 +0530

    2nd

--- app/src/main/java/com/learnandroid/foodie/ChooeseLocationActivity.kt
+++ app/src/main/java/com/learnandroid/foodie/ChooeseLocationActivity.kt
@@ -2,12 +2,19 @@ package com.learnandroid.foodie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.learnandroid.foodie.databinding.ActivityChooeseLocationBinding
 
 class ChooeseLocationActivity : AppCompatActivity() {
-    private val binding: ActivityChooeseLocationBinding
+    private val binding: ActivityChooeseLocationBinding by lazy {
+        ActivityChooeseLocationBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chooese_location)
+        setContentView(binding.root)
+        val locationList = arrayOf("Gujarat", "Maharashtra", "Rajasthan", "Goa")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,locationList)
+        val autoCompleteTextView = binding.tvLocationList
+        autoCompleteTextView.setAdapter(adapter)
     }
 }
